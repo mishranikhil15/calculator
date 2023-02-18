689bf183fd518f94aeaba710c5405cbd9f94bfa8
Nikhil Kumar <vashishtnikhil158@gmail.com>
2023-02-19 02:29:37 +0530

    calci5

--- index.js
+++ index.js
@@ -2,18 +2,46 @@
 
 //  import the crypto module
 
-
+const crypto= require("crypto")
 
 //  get a commands using process.argv
 
 
+
 // complete the  function
+function add(argv1,argv2){
+  return (+argv1)+(+argv2)
+}
+
+function sub(argv1,argv2){
+  return (+argv1)-(+argv2)
+}
+
+function mult(argv1,argv2){
+  return Number(argv1)*Number(argv2)
+}
+
+function divide(argv1,argv2){
+  return Number(argv1)/Number(argv2)
+}
+
+function sin(argv1,argv2){
+  return Math.sin(Number(argv1))
+}
+
+function cos(argv1,argv2){
+  return Math.cos(Number(argv1))
+}
+
+function tan(argv1,argv2){
+  return Math.tan(Number(argv1))
+}
 
 const process= require("process");
 
 const cmd = process.argv;
 
-console.log(cmd)
+// console.log(cmd)
 // console.log(cmd[2],cmd[3],cmd[4])
 
 const command= cmd[2];
@@ -21,45 +49,33 @@ const argv1= cmd[3];
 const argv2= cmd[4];
 
 if(command=="add"){
-  function add(argv1,argv2){
-    return (+argv1)+(+argv2)
-  }
+ 
   console.log(add(argv1,argv2))
 
 }else if(command=="sub"){
-  function sub(argv1,argv2){
-    return (+argv1)-(+argv2)
-  }
+  
   console.log(sub(argv1,argv2))
 
 }else if(command=="mult"){
-  function mult(argv1,argv2){
-    return (+argv1)*(+argv2)
-  }
+ 
   console.log(mult(argv1,argv2))
 
-}else if(command=="div"){
-  function div(argv1,argv2){
-    return (+argv1)/(+argv2)
-  }
-  console.log(div(argv1,argv2))
+}else if(command=="divide"){
+  
+  console.log(divide(argv1,argv2))
+
 }else if(command=="sin"){
-  function sin(argv1,argv2){
-    return Math.sin(+argv1)
-  }
+ 
   console.log(sin(argv1,argv2))
+
 }else if(command=="cos"){
-  function cos(argv1,argv2){
-    return Math.cos(+argv1)
-  }
+ 
   console.log(cos(argv1,argv2))
+
 }else if(command=="tan"){
-  function tan(argv1,argv2){
-    return Math.tan(+argv1)
-  }
+ 
   console.log(tan(argv1,argv2))
 }
-
 
 
 // switch (operation) {
